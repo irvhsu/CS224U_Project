86a2ecabf852e5e8b039e6c622e2936afcea53d5
Irving Hsu <irvhsu@stanford.edu>
2016-05-24 13:31:51 -0700

    Add script to compute average BLEU scores

---
+++ get_bleu_scores.py
@@ -0,0 +1,39 @@
+from constants import *
+from nltk.translate import bleu_score
+
+'''
+Function: get_bleu_scores
+-------------------------
+Takes in a pair of files -- one containing the ground truth hypotheses, and the other containing the
+generated hypotheses. Computes the BLEU score for each pair of sentences, and outputs the average
+BLEU score at the end.
+'''
+def get_bleu_scores(infile1, infile2, n=1):
+    print "\nComputing BLEU score for n={}...".format(n)
+    f1 = open(infile1)
+    f2 = open(infile2)
+
+    sentences_f1 = f1.readlines()
+    sentences_f2 = f2.readlines()
+
+    bleu_scores = []
+
+    sentence_pairs = zip(sentences_f1, sentences_f2)
+
+    for pair in sentence_pairs:
+        # Ground truth hypothesis
+        reference = pair[0][:-1]
+        # Generated hypothesis
+        hypothesis = pair[1][:-1]
+
+        score = bleu_score._modified_precision([reference], hypothesis, n)
+        bleu_scores.append(float(score))
+
+    avg_bleu_score = sum(bleu_scores) / float(len(bleu_scores))
+    print "Average BLEU score: {}\n".format(avg_bleu_score)
+
+if __name__ == '__main__':
+    infile1 = "test_files/premises.txt"
+    infile2 = "test_files/hypotheses.txt"
+    get_bleu_scores(infile1, infile2, n=1)
+    get_bleu_scores(infile1, infile2, n=2)
