c3e11c962e559e3dea98b76aae9c550bba9f8ca7
Irving Hsu <irvhsu@stanford.edu>
2016-06-04 17:01:49 -0700

    Write SICK premises and hypotheses to separate files

---
+++ write_pr_and_hp_sick.py
@@ -0,0 +1,63 @@
+from constants import *
+from read_sick_data import *
+
+'''
+Function: process_sick_data
+---------------------------
+Reads the snli data from the given filename
+and writes the premises and hypotheses of the
+entailments into separate output files.
+'''
+
+
+def process_sick_data(infile):
+    prefix = infile.split('.txt')[0]
+    outfile_pr = prefix + '.premise'
+    outfile_hp = prefix + '.hypothesis'
+
+    print 'Reading in data from ' + infile + '...'
+    f = open(infile, 'r')
+    # List of premises
+    premise_list = []
+    # List of hypotheses
+    hypothesis_list = []
+    for line in f:
+        # Split input line by tabs
+        split_line = line.split('\t')
+        # Only look at entailments
+        entailment_label = split_line[3]
+        if entailment_label != 'ENTAILMENT': continue
+        # Put spaces before punctuation marks
+        premise = format_sent(split_line[1], False)
+        hypothesis = format_sent(split_line[2], False)
+
+        # Add to growing premise and hypothesis lists
+        premise_list.append(premise)
+        hypothesis_list.append(hypothesis)
+    f.close()
+
+    # Write the premises and hypotheses to their corresponding output files
+    write_to_file(outfile_pr, premise_list)
+    write_to_file(outfile_hp, hypothesis_list)
+
+    print 'Done!'
+
+
+'''
+Function: write_to_file
+-----------------------
+Writes the data in the given list to the given output file,
+line by line.
+'''
+
+
+def write_to_file(outfile, data_list):
+    f = open(outfile, 'w')
+    for sent in data_list:
+        f.write(sent + '\n')
+    f.close()
+
+
+if __name__ == '__main__':
+    print "Processing SICK data..."
+    process_sick_data(SICK_DATA_FILE)
